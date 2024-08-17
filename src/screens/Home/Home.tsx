09c275af1987c769592cd05517747f5c2ecc11e4
fmacedosantos <felipemacedo986@gmail.com>
2024-08-17 18:11:21 -0300

    Criando a Home e adicionando a ProfileImage

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { ProfileImage } from '../../components/ProfileImage/ProfileImage';
+
+export function Home() {
+  return (
+    <View>
+        <ProfileImage text='Felipe Macedo dos Santos'/>
+    </View>
+  );
+}
