9040ff1f4f402f1f621a381beddc6042fe50e7e2
fmacedosantos <felipemacedo986@gmail.com>
2024-08-18 00:46:39 -0300

    Adicionando ícones ao menu

--- src/routes/bottom-tabs.routes.tsx
+++ src/routes/bottom-tabs.routes.tsx
@@ -2,6 +2,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "../screens/Home/Home";
 import { Skills } from "../screens/Skills/Skills";
 
+import Entypo from '@expo/vector-icons/Entypo';
+
 const {Navigator, Screen} = createBottomTabNavigator()
 
 export function BottomTabRoutes(){
@@ -9,11 +11,17 @@ export function BottomTabRoutes(){
         <Navigator>
             <Screen name="Home" component={Home}
             options={{
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <Entypo name="home" size={24} color="black" />
+                )
             }}/>
             <Screen name="Skills" component={Skills}
             options={{
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <Entypo name="code" size={24} color="black" />
+                )
             }}/>
         </Navigator>
     )
