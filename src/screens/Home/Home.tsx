84975895034086a6c863ccdc8ebbcebbb46acd2a
fmacedosantos <felipemacedo986@gmail.com>
2024-08-18 00:16:41 -0300

    Arrumando o conteúdo com a navegação

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollViewComponent, View } from 'react-native';
 import { ProfileImage } from '../../components/ProfileImage/ProfileImage';
 import { LinkButton } from '../../components/LinkButton/LinkButton';
 
@@ -9,12 +9,10 @@ export function Home() {
   return (
     <View style={styles.container}>
         <ProfileImage text='Felipe Macedo dos Santos'/>
-        <View>
-          <LinkButton text='LINKEDIN' url='https://www.linkedin.com/in/felipe-macedo-dos-santos-37264a1b5/'/>
-          <LinkButton text='GITHUB' url='https://github.com/fmacedosantos'/>
-          <LinkButton text='LEET CODE' url='https://leetcode.com/u/fmacedosantos/'/>
-          <LinkButton text='YOUTUBE' url='https://www.youtube.com/@fmacedosantos'/>
-        </View>
+        <LinkButton text='LINKEDIN' url='https://www.linkedin.com/in/felipe-macedo-dos-santos-37264a1b5/'/>
+        <LinkButton text='GITHUB' url='https://github.com/fmacedosantos'/>
+        <LinkButton text='LEET CODE' url='https://leetcode.com/u/fmacedosantos/'/>
+        <LinkButton text='YOUTUBE' url='https://www.youtube.com/@fmacedosantos'/>
 
     </View>
   );
