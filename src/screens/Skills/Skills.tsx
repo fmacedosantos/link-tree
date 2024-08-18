cffad98438a24a2983a9ea038b3817a63c200d41
fmacedosantos <felipemacedo986@gmail.com>
2024-08-18 00:47:23 -0300

    Aplicando o componente

--- src/screens/Skills/Skills.tsx
+++ src/screens/Skills/Skills.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { styles } from './Styles';
+import { ProfileImage } from '../../components/ProfileImage/ProfileImage';
+import { SkillText } from '../../components/SkillText/SkillText';
 
 export function Skills() {
   return (
     <View style={styles.container}>
-
+      <ProfileImage text='Minhas Habilidades'/>
+      <SkillText name={'html5'} text='HTML5' numberStars={5}/>
+      <SkillText name={'css3'} text='CSS' numberStars={3}/>
     </View>
   );
 }
