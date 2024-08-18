4ac4853b56773c9b6695002aabde6e9d57255420
fmacedosantos <felipemacedo986@gmail.com>
2024-08-17 23:51:03 -0300

    Estilizando e fixando o layout

--- src/components/ProfileImage/Styles.tsx
+++ src/components/ProfileImage/Styles.tsx
@@ -2,9 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex:0.6,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
   image: {
     width: 150,
