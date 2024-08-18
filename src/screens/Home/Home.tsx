3ddd87257f26a968c3d43f424b8000eebdc355ea
fmacedosantos <felipemacedo986@gmail.com>
2024-08-17 23:50:40 -0300

    Aplicando o componente LinkButton

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View } from 'react-native';
 import { ProfileImage } from '../../components/ProfileImage/ProfileImage';
+import { LinkButton } from '../../components/LinkButton/LinkButton';
+
+import { styles } from './Styles';
 
 export function Home() {
   return (
-    <View>
+    <View style={styles.container}>
         <ProfileImage text='Felipe Macedo dos Santos'/>
+        <View>
+          <LinkButton text='LINKEDIN' url='https://www.linkedin.com/in/felipe-macedo-dos-santos-37264a1b5/'/>
+          <LinkButton text='GITHUB' url='https://github.com/fmacedosantos'/>
+          <LinkButton text='LEET CODE' url='https://leetcode.com/u/fmacedosantos/'/>
+          <LinkButton text='YOUTUBE' url='https://www.youtube.com/@fmacedosantos'/>
+        </View>
+
     </View>
   );
 }
