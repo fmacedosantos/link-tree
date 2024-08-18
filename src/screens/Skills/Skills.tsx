5210c8036d950d1f9ac4a016de3c65d4b5b1902b
fmacedosantos <felipemacedo986@gmail.com>
2024-08-18 00:02:46 -0300

    Criando a tela Skills

---
+++ src/screens/Skills/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './Styles';
+
+export function Skills() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
