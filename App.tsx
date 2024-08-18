2c15b22ed36d0b2bbb047cfa5e9faf519b4a6fe5
fmacedosantos <felipemacedo986@gmail.com>
2024-08-18 00:03:23 -0300

    Aplicando o Routes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screens/Home/Home';
+import { StyleSheet } from 'react-native';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home/>
-      <StatusBar style="auto" />
-    </View>
+    <Routes/>
   );
 }
 
